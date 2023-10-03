b186531dfd2da9dbb10f5abb8275d838df2f7de3
Monde Sineke <nsineke@gmail.com>
2023-10-03 15:04:16 +0200

    🐳 chore(package): Add netlify next

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,8 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
+        <LoginBtn />
+
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -40,8 +42,6 @@ export default function Home() {
           priority
         />
       </div>
-
-      <LoginBtn />
     </main>
   );
 }
